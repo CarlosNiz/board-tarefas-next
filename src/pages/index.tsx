2ad310af6d33d65f04059300e33b66bff7a44c3f
Carlos Niz <c4rlos.niz@gmail.com>
2025-05-04 21:16:05 -0300

    feat: creating main page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
 import Head from "next/head";
 import styles from "../../styles/home.module.css";
- 
+import Image from "next/image"; 
+import heroImg from "../../public/assets/hero.png";
+
 export default function Home() {
   return (
     <div className={styles.container}>
@@ -9,7 +11,31 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
 
-      <h1>Meu projeto</h1>
+      <main className={styles.main}>
+        <div className={styles.logoContent}>
+          <Image 
+            className={styles.hero}
+            alt="Logo Tarefas +"
+            src={heroImg}
+            priority
+          />
+        </div>
+
+        <h1 className={styles.title}>
+          Sistema feito para você organizar <br/>
+          seus estudos e tarefas 
+        </h1>
+
+        <div className={styles.infoContent}>
+          <section className={styles.box}>
+            <span>+12 posts</span>
+          </section>
+          <section className={styles.box}>
+            <span>+90 comentários</span>
+          </section>
+        </div>
+
+      </main>
     </div>
   );
 }
