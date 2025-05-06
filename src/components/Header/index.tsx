01e0afc820bd2a0fc529198976683be55db7e26c
Carlos Niz <c4rlos.niz@gmail.com>
2025-05-06 11:53:52 -0300

    feat: create login

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,10 @@
+import { useSession, signIn, signOut } from 'next-auth/react';
 import styles from './styles.module.css'
 import Link from 'next/link'
 
 export function Header() {
+    const { data: session, status } = useSession();
+
     return(
         <header className={styles.header}>
             <section className={styles.content}>
@@ -9,10 +12,24 @@ export function Header() {
                     <Link href="/"> 
                         <h1 className={styles.logo}>Tarefas<span>+</span></h1>
                     </Link>
-                    <Link className={styles.link} href="/dashboard">Meu Painel</Link>
+                    { session?.user && (
+                        <Link className={styles.link} href="/dashboard">
+                            Meu Painel
+                        </Link>
+                    ) }
                 </nav>
 
-                <button className={styles.loginButton}>Acessar</button>
+                { status == "loading" ? (
+                    <></>
+                ) : session ? (
+                    <button className={styles.loginButton} onClick={ () => signOut() }>
+                        Olá {session?.user?.name}
+                    </button>
+                ): (
+                    <button className={styles.loginButton} onClick={ () => signIn("google") }>
+                        Acessar
+                    </button>
+                )} 
             </section>
         </header>
     )
